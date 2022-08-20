495ad32084d604bce8d976cf428b95298a375126
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-08-20 20:02:26 +0700

    Fixed CORS

--- api/index.js
+++ api/index.js
@@ -40,6 +40,11 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://immifit.vercel.app/");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
 
 // /activities
 const activityRoutes = require("../Routes/activitiesRoute");
