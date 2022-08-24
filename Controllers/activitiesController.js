0b54784ca0326fafe12b6546fa580e12f98fc030
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-08-25 02:57:55 +0700

    Edit By Id

--- Controllers/activitiesController.js
+++ Controllers/activitiesController.js
@@ -11,9 +11,9 @@ const getAllActivities = async (req, res, next) => {
 
 const getActivityById = async (req, res, next) => {
   const { activity_id } = req.params;
-  console.log(activity_id);
+  
   const activity = await Activities.findOne({ activity_id });
-  console.log(activity)
+  
   if (!activity) {
     return res.status(404).send({ message: "Activity not found" });
   }
@@ -64,24 +64,42 @@ const createActivity = async (req, res, next) => {
 };
 
 const editActivityById = async (req, res, next) => {
-  const { activity_type, title, date, duration, description } = req.body;
-
-  if (activity_type) req.activity.activity_type = activity_type;
-  if (title) req.activity.title = title;
-  if (date) req.activity.date = date;
-  if (duration) req.activity.duration = duration;
-  if (description) req.activity.description = description;
-
-  await req.activity.save();
-
-  res.send(req.activity);
+  const user = await User.findOne({
+    username: req.body.username,
+  });
+  const fileStr = req.body.img.data;
+  const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+    upload_preset: "immifit",
+  });
+  console.log(uploadResponse);
+  try {
+    const newActivity = new Activities({
+      img: {
+        name: req.body.img.name,
+        id: '',
+        url: '',
+        contentType: req.body.img.contentType,
+      },
+      activity_id: uuidv4(),
+      username: user.username,
+      user_id: user.user_id,
+      ...req.body,
+    });
+    newActivity.img.id = uploadResponse.asset_id;
+    newActivity.img.url = uploadResponse.secure_url;
+    await newActivity.save();
+    res.status(200).send(newActivity)
+  } catch (error) {
+    res.status(400).send(error);
+    console.log(error);
+  }
 };
 
 const removeActivityById = async (req, res, next) => {
   const { activity_id } = req.params;
-  console.log(activity_id);
+  
   const activity = await Activities.findOne({ activity_id });
-  console.log(activity)
+  
   if (!activity) {
     return res.status(404).send({ message: "Activity not found" });
   }
